c1682a1053d07da78dba63c540cdabaa8a3cc8c3
BlueSaNGG <BlueSaNGG@163.com>
2023-05-24 18:26:50 -0500

    lazy initialization of retrofit and stackoverflowApi

--- app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
@@ -8,12 +8,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class AppCompositionRoot {
-    private val retrofit: Retrofit = Retrofit.Builder().baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
 
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(
-        StackoverflowApi::class.java
-    )
+    private val stackoverflowApi: StackoverflowApi by lazy {
+        retrofit.create(
+            StackoverflowApi::class.java
+        )
+    }
 
     val fetchQuestionsUseCase: FetchQuestionsUseCase
         get() = FetchQuestionsUseCase(stackoverflowApi)
