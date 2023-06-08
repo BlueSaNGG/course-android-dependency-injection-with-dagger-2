7682f897b551d9349fdb48b98591e32d4f542bf5
BlueSaNGG <BlueSaNGG@163.com>
2023-06-08 11:00:17 -0500

    revert to providing screensNavigator from activity module to manage scoping in one place

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/activity/ActivityModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/activity/ActivityModule.kt
@@ -1,6 +1,7 @@
 package com.techyourchance.dagger2course.common.dependencyInjection.activity
 
 import androidx.appcompat.app.AppCompatActivity
+import com.techyourchance.dagger2course.screens.common.ScreensNavigator
 import dagger.Module
 import dagger.Provides
 
@@ -8,6 +9,10 @@ import dagger.Provides
 class ActivityModule(
     private val activity: AppCompatActivity
 ) {
+
+    @Provides
+    @ActivityScope
+    fun screensNavigator(activity: AppCompatActivity) = ScreensNavigator(activity)
 
     @Provides
     fun fragmentManager() = activity.supportFragmentManager
